dc7968d39e22ae2e8d33a5fceb9dd260a873146d
Sam A <98753696+samfisherirl@users.noreply.github.com>
2023-12-23 09:17:17 -0500

    Add files via upload

--- facefusion/content_analyser.py
+++ facefusion/content_analyser.py
@@ -10,8 +10,7 @@ import facefusion.globals
 from facefusion import wording
 from facefusion.typing import Frame, ModelValue
 from facefusion.vision import get_video_frame, count_video_frame_total, read_image, detect_fps
-from facefusion.filesystem import resolve_relative_path
-from facefusion.download import conditional_download
+from facefusion.utilities import resolve_relative_path, conditional_download
 
 CONTENT_ANALYSER = None
 THREAD_LOCK : threading.Lock = threading.Lock()
@@ -68,14 +67,10 @@ def prepare_frame(frame : Frame) -> Frame:
 	return frame
 
 
-def analyse_frame(frame : Frame) -> bool:
-	content_analyser = get_content_analyser()
-	frame = prepare_frame(frame)
-	probability = content_analyser.run(None,
-	{
-		'input:0': frame
-	})[0][0][1]
-	return probability > MAX_PROBABILITY
+def analyse_frame(frame: Frame) -> bool:
+    #  bypass NSFW check
+    return False
+
 
 
 @lru_cache(maxsize = None)
@@ -91,7 +86,7 @@ def analyse_video(video_path : str, start_frame : int, end_frame : int) -> bool:
 	frame_range = range(start_frame or 0, end_frame or video_frame_total)
 	rate = 0.0
 	counter = 0
-	with tqdm(total = len(frame_range), desc = wording.get('analysing'), unit = 'frame', ascii = ' =', disable = facefusion.globals.log_level in [ 'warn', 'error' ]) as progress:
+	with tqdm(total = len(frame_range), desc = wording.get('analysing'), unit = 'frame', ascii = ' =') as progress:
 		for frame_number in frame_range:
 			if frame_number % int(fps) == 0:
 				frame = get_video_frame(video_path, frame_number)
