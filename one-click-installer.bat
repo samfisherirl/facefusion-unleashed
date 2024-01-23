420c5e3cb983793b734960ab977992708527700e
Sam A <98753696+samfisherirl@users.noreply.github.com>
2024-01-22 20:15:11 -0500

    Update one-click-installer.bat

--- one-click-installer.bat
+++ one-click-installer.bat
@@ -1,19 +1,42 @@
 @echo off
 setlocal enabledelayedexpansion
 
+:: Set the working directory
+set "WORKING_DIR=%CD%"
+set "VENVPY=%WORKING_DIR%\venv\Scripts\python.exe"
+set "ACTIVATE=%WORKING_DIR%\venv\Scripts\activate.bat"
+set "INSTALL=%WORKING_DIR%\facefusion\install.py"
+set "RUN=%WORKING_DIR%\facefusion\run.py"
+
+:: Check if Git is installed and in PATH
+where git >nul 2>&1
+if errorlevel 1 (
+    echo Git is not installed or not in PATH.
+    exit /b 1
+)
+
+:: Clone the repository using Git
+echo Cloning facefusion repository...
+git clone https://github.com/facefusion/facefusion
+
 :: Define Python version
 set "PYTHON_VERSION=3.10.11"
 
-:: Default Python location
+:: Initialize PYTHON_DIR variable
 set "PYTHON_DIR="
 
-:: Find Python 3.10 location
-for %%d in ("%LOCALAPPDATA%\Programs\Python\Python310" "C:\ProgramData\Python310") do (
-    if exist "%%~d\python.exe" (
-        set "PYTHON_DIR=%%~d"
-        goto FoundPython
-    )
+:: Find Python 3.10 location in LOCALAPPDATA
+if exist "%LOCALAPPDATA%\Programs\Python\Python310\python.exe" (
+    set "PYTHON_DIR=%LOCALAPPDATA%\Programs\Python\Python310"
+    goto FoundPython
 )
+
+:: Find Python 3.10 location in ProgramData
+if exist "C:\ProgramData\Python310\python.exe" (
+    set "PYTHON_DIR=C:\ProgramData\Python310"
+    goto FoundPython
+)
+
 
 :: If Python is not found, download and extract it
 if not defined PYTHON_DIR (
@@ -22,26 +45,34 @@ if not defined PYTHON_DIR (
     echo Extracting Python...
     powershell -Command "Expand-Archive -LiteralPath 'python-%PYTHON_VERSION%-embed-amd64.zip' -DestinationPath '.\py'"
     set "PYTHON_DIR=%CD%\py"
-    set "PYTHONEXE=%PYTHON_DIR%\python.exe"
-) else (
-    set "PYTHONEXE=%PYTHON_DIR%\python.exe"
 )
 
 :FoundPython
+:: Set PYTHON_EXE variable based on the identified PYTHON_DIR
+set "PYTHON_EXE=!PYTHON_DIR!\python.exe"
+echo Found Python in %PYTHON_DIR%
+echo Executable is %PYTHON_EXE%
+  
 :: Create virtual environment using located or downloaded Python
 echo Creating a virtual environment...
-"%PYTHONEXE%" -m venv "%PYTHON_DIR%\venv"
+call "%PYTHON_EXE%" -m venv venv
 
-:: Activate virtual environment
-call "%PYTHON_DIR%\venv\Scripts\activate.bat"
-
+:: Activate the virtual environment
+echo Activating the virtual environment...
+call "%ACTIVATE%"
 :: Install your python package with desired options
-"%PYTHON_DIR%\python.exe" install.py --torch cuda --onnxruntime cuda
+echo Installing packages...
+@echo on
+call "%VENVPY%" "%INSTALL%" --torch cuda --onnxruntime cuda
+:: Download content_analyser.py and copy to /facefusion/facefusion/
+echo Downloading content_analyser.py...
+@echo on
+powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/samfisherirl/facefusion-unleashed-one-click-installer/master/facefusion/content_analyser.py' -OutFile '.\facefusion\facefusion\content_analyser.py'"
 
-:: Run
-"%PYTHON_DIR%\python.exe" run.py --torch cuda --onnxruntime cuda
-
+:: Run the program
+echo Running the program...
+call "%VENVPY%" "%RUN%" --torch cuda --onnxruntime cuda 
 :: Deactivate virtual environment
-:: call "%PYTHON_DIR%\venv\Scripts\deactivate.bat"
+:: "%WORKING_DIR%\venv\Scripts\deactivate.bat"
 
 echo Script completed.
